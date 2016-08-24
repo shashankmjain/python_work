4d1e25636f8da18f07a9f083b6ecd3e4179d482c
Jain <I074560@global.corp.sap>
2016-08-24 15:14:52 +0530

    added sample python client

--- app.py
+++ app.py
@@ -38,4 +38,14 @@ for _ in range(100):
 producer.flush()
 
 # configure multiple retries
-producer = KafkaProducer(retries=5)
+#producer = KafkaProducer(retries=5)
+print ('message produced')
+consumer = KafkaConsumer('demo-topic',
+                         group_id='my-group',
+                         bootstrap_servers=['192.168.27.185:9092'])
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
