2522d54085c1ffe54665ee8f916703d882cc28e5
Jain <I074560@global.corp.sap>
2016-08-24 14:36:55 +0530

    added sample python client

--- app.py
+++ app.py
@@ -16,3 +16,28 @@ producer = SimpleProducer(client,
                           async=True,
                           batch_send_every_n=20,
                           batch_send_every_t=60)
+						  
+						client = SimpleClient('192.168.27.185:9092')
+consumer = SimpleConsumer(client, "my-group", "demo-topic")
+for message in consumer:
+    # message is raw byte string -- decode if necessary!
+    # e.g., for unicode: `message.decode('utf-8')`
+    print(message)
+
+
+# Use multiprocessing for parallel consumers
+from kafka import MultiProcessConsumer
+
+# This will split the number of partitions among two processes
+consumer = MultiProcessConsumer(client, "my-group", "demo-topic", num_procs=2)
+
+# This will spawn processes such that each handles 2 partitions max
+
+
+for message in consumer:
+    print(message)
+
+for message in consumer.get_messages(count=5, block=True, timeout=4):
+    print(message)
+
+client.close()
